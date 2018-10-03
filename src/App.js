ca14a30d95ee6cf9a21e16a9ca8ba6f05c9ec50b
Kate Speed <katespeed@gmail.com>
2018-10-02 20:56:02 -0400

    Initialize map and add basic styling

--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  componentDidMount() {
+    this.renderMap()
+  }
+
+  renderMap = () => {
+    loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyDAKvy5lm0G0jkaL6-OwZRqZtv9d4Cgqqw&callback=initMap")
+    window.initMap = this.initMap
+  }
+
+  initMap = () => {
+    const map = new window.google.maps.Map(document.getElementById('map'), {
+      center: {lat: -34.397, lng: 150.644},
+      zoom: 8
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <main>
+        <div id="map"></div>
+      </main>
     );
   }
 }
+
+function loadScript(url) {
+  // find the first script tag in index.html
+  var index = window.document.getElementsByTagName('script')[0]
+  // create a new script tag and add properties
+  var script = window.document.createElement('script')
+  script.src = url
+  script.async = true
+  script.defer = true
+  // put the new script tag before the first one that was found
+  index.parentNode.insertBefore(script, index)
+}
+
 
 export default App;
