54c83fecc5a5a07181267db3ad38e5aef1663f04
Kate Speed <katespeed@gmail.com>
2018-10-17 20:49:06 -0400

    Move origtrail data and img imports to separate file

---
+++ src/OrigTrails.js
@@ -0,0 +1,225 @@
+import img1 from './img/img1.jpg';
+import img2 from './img/img2.jpg';
+import img3 from './img/img3.jpg';
+import img4 from './img/img4.jpg';
+import img5 from './img/img5.jpg';
+import img6 from './img/img6.jpg';
+import img7 from './img/img7.jpg';
+import img8 from './img/img8.jpg';
+import img9 from './img/img9.jpg';
+import img10 from './img/img10.jpg';
+import img11 from './img/img11.jpg';
+import img12 from './img/img12.jpg';
+import img13 from './img/img13.jpg';
+import img14 from './img/img14.jpg';
+import img15 from './img/img15.jpg';
+import img16 from './img/img16.jpg';
+import img17 from './img/img17.jpg';
+import img18 from './img/img18.jpg';
+import img19 from './img/img19.jpg';
+import img20 from './img/img20.jpg';
+
+export default {
+    origTrails: [
+      {
+        "id": 7010880,
+        "name": "Battle Branch / Bolin Creek Loop",
+        "summary": "Wilderness in the heart of Chapel Hill adjacent to UNC Chapel Hill Campus, with plenty of hills.",
+        "difficulty": "greenBlue",
+        "imgSmall": img1,
+        "length": 3.7,
+        "longitude": -79.0323,
+        "latitude": 35.9259
+      },
+      {
+        "id": 7011748,
+        "name": "Crow Branch Overlook Loop Trail",
+        "summary": "Great forested singletrack trail that's very easily accessible to Chapel Hill and Carrboro.",
+        "difficulty": "greenBlue",
+        "imgSmall": img2,
+        "length": 4,
+        "longitude": -79.0681,
+        "latitude": 35.9372
+      },
+      {
+        "id": 7022521,
+        "name": "Johnston Mill Loop",
+        "summary": "This pleasant loop is a scenic alternative for those wishing to avoid more crowded parks.",
+        "difficulty": "greenBlue",
+        "imgSmall": img3,
+        "length": 2.9,
+        "longitude": -79.0541,
+        "latitude": 35.9954
+      },
+      {
+        "id": 7022400,
+        "name": "Duke Korstian Loop",
+        "summary": "A good all-weather loop.",
+        "difficulty": "greenBlue",
+        "imgSmall": img4,
+        "length": 4.2,
+        "longitude": -79.0226,
+        "latitude": 35.978
+      },
+      {
+        "id": 7024716,
+        "name": "Korstian Foot Trail Loop",
+        "summary": "This loop combines several of Korstian's foot trails.",
+        "difficulty": "blue",
+        "imgSmall": img5,
+        "length": 6.9,
+        "longitude": -79.0225,
+        "latitude": 35.9781
+      },
+      {
+        "id": 7042865,
+        "name": "Brumley Forest Nature Preserve Loop",
+        "summary": "An extended loop trail within Brumley Forest Nature Preserve.",
+        "difficulty": "greenBlue",
+        "imgSmall": img6,
+        "length": 5.8,
+        "longitude": -79.0593,
+        "latitude": 36.0312
+      },
+      {
+        "id": 7004485,
+        "name": "Al Buehler and Sally Meyerhoff Trails",
+        "summary": "A very well-maintained trail circumscribing the Washington Duke golf course.",
+        "difficulty": "blue",
+        "imgSmall": img7,
+        "length": 3.7,
+        "longitude": -78.9523,
+        "latitude": 35.9971
+      },
+      {
+        "id": 7037167,
+        "name": "Eno Outer Loop",
+        "summary": "This longer loop visits the three highest points in the park",
+        "difficulty": "blue",
+        "imgSmall": img8,
+        "length": 7.4,
+        "longitude": -79.0062,
+        "latitude": 36.0739
+      },
+      {
+        "id": 7022705,
+        "name": "New Hope Overlook - Red-Blue Trails",
+        "summary": "This trail passes through woodlands to reach the Jordan Lake shoreline and loops back through woodlands.",
+        "difficulty": "greenBlue",
+        "imgSmall": img9,
+        "length": 5.1,
+        "longitude": -79.048,
+        "latitude": 35.6825
+      },
+      {
+        "id": 7015858,
+        "name": "MST: West Point to Pump Station",
+        "summary": "Take the Mountains-to-Sea Trail along the Eno River from West Point to the Pump Station and back.",
+        "difficulty": "greenBlue",
+        "imgSmall": img10,
+        "length": 9.3,
+        "longitude": -78.9081,
+        "latitude": 36.069
+      },
+      {
+        "id": 7043491,
+        "name": "Poe's Ridge Trail",
+        "summary": "This is a 4-mile multi-use loop along Jordan Lake between the Visitor Assistance Center and Poe's Ridge boat ramp.",
+        "difficulty": "blue",
+        "imgSmall": img11,
+        "length": 3.7,
+        "longitude": -79.0704,
+        "latitude": 35.6548
+      },
+      {
+        "id": 7022294,
+        "name": "Lake Trail",
+        "summary": "The Lake Trail is a great urban escape and one of the best in the Triangle area!",
+        "difficulty": "blue",
+        "imgSmall": img12,
+        "length": 6.4,
+        "longitude": -78.7954,
+        "latitude": 35.8421
+      },
+      {
+        "id": 7043428,
+        "name": "Jimmy Rogers Road Out-and-Back",
+        "summary": "A section of the MST along Falls Lake. The boardwalk by Little Lick Creek Bridge is the main highlight.",
+        "difficulty": "greenBlue",
+        "imgSmall": img13,
+        "length": 7.6,
+        "longitude": -78.7777,
+        "latitude": 36.0132
+      },
+      {
+        "id": 7024662,
+        "name": "Bond Park Lake Loop",
+        "summary": "An easy, short loop hike around the lake on singletrack, mulched, and paved trail.",
+        "difficulty": "blue",
+        "imgSmall": img14,
+        "length": 3,
+        "longitude": -78.8293,
+        "latitude": 35.7846
+      },
+      {
+        "id": 7010450,
+        "name": "Company Mill Trail",
+        "summary": "A great get away in the heart of the triangle with some scenic hiking down on Crabtree Creek.",
+        "difficulty": "blue",
+        "imgSmall": img15,
+        "length": 5.1,
+        "longitude": -78.7599,
+        "latitude": 35.8365
+      },
+      {
+        "id": 7011687,
+        "name": "Turkey Creek/Cedar Ridge Loop",
+        "summary": "A good rolling loop trail through established NC forest with water features.",
+        "difficulty": "greenBlue",
+        "imgSmall": img16,
+        "length": 5.4,
+        "longitude": -78.7251,
+        "latitude": 35.843
+      },
+      {
+        "id": 7020965,
+        "name": "Horton Grove Nature Preserve Loop",
+        "summary": "This loop combines the varied terrain of several trails to make a great longer hike.",
+        "difficulty": "greenBlue",
+        "imgSmall": img17,
+        "length": 6,
+        "longitude": -78.8386,
+        "latitude": 36.1278
+      },
+      {
+        "id": 7043936,
+        "name": "Rolling View Marina View Out-and-Back",
+        "summary": "A pleasant section of the MST following the lake early on. The highlight is seeing Rolling View Marina coming into view.",
+        "difficulty": "greenBlue",
+        "imgSmall": img18,
+        "length": 8.2,
+        "longitude": -78.6891,
+        "latitude": 36.0124
+      },
+      {
+        "id": 7044858,
+        "name": "Cedarock Park Tour",
+        "summary": "This hike combines the best that Cedarock Park has to offer.",
+        "difficulty": "blue",
+        "imgSmall": img19,
+        "length": 7.1,
+        "longitude": -79.4465,
+        "latitude": 35.9896
+      },
+      {
+        "id": 7013179,
+        "name": "White Pines Loop",
+        "summary": "A hike with everything: dramatic views from atop bluffs, scenic rivers, and unique ecosystems.",
+        "difficulty": "greenBlue",
+        "imgSmall": img20,
+        "length": 2.4,
+        "longitude": -79.1607,
+        "latitude": 35.6145
+      }
+  ]
+  }
